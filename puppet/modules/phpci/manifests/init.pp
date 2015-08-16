cbd61df9306cab4ecfe731e11335d45464b99987
Neil Brayfield <neil@brayfield.uk>
2015-08-16 19:44:37 +0100

    added removal of phpci

--- puppet/modules/phpci/manifests/init.pp
+++ puppet/modules/phpci/manifests/init.pp
@@ -10,52 +10,62 @@ class phpci (
     $admin_password = 'phpci',
     $install_path = '/home/vagrant/phpci',
     $composer_home = '/home/vagrant',
+    $ensure = 'present',
 ) {
     require mysql::server
     require php
     require php::cli
 
+    if $ensure == 'present' {
 
-    exec { 'phpci-create':
-        command => "composer create-project block8/phpci ${install_path} --keep-vcs --no-dev",
-        path    => '/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin',
-        environment => "COMPOSER_HOME=${composer_home}",
-        user => 'vagrant',
-        creates  => "${install_path}/console",
+        exec { 'phpci-create':
+            command => "composer create-project block8/phpci ${install_path} --keep-vcs --no-dev",
+            path    => '/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin',
+            environment => "COMPOSER_HOME=${composer_home}",
+            user => 'vagrant',
+            creates  => "${install_path}/console",
+        }
+
+        exec { 'phpci-composer':
+            command => "composer install",
+            path    => '/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin',
+            environment => "COMPOSER_HOME=${composer_home}",
+            user => 'vagrant',
+            cwd => $install_path,
+            subscribe   => Exec['phpci-create'],
+            refreshonly => true
+        }
+
+        exec { 'phpci-install':
+            command => "console phpci:install --url='${url}' --db-host='${db_host}' --db-name='${db_name}' --db-user='${db_user}' --db-pass='${db_password}' --admin-name='${admin_name}' --admin-mail='${admin_email}' --admin-pass='${admin_password}'",
+            path    => "/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin:${install_path}",
+            cwd => $install_path,
+            user => 'vagrant',
+            subscribe   => Exec['phpci-composer'],
+            refreshonly => true,
+        }
+
+
+        Mysql::Db[$db_name] -> Exec['phpci-create']
+
+
+    } else {
+        file { $install_path:
+            ensure => $ensure,
+            force => true,
+        }
     }
 
-
-    exec { 'phpci-composer':
-        command => "composer install",
-        path    => '/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin',
-        environment => "COMPOSER_HOME=${composer_home}",
-        user => 'vagrant',
-        cwd => $install_path,
-        subscribe   => Exec['phpci-create'],
-        refreshonly => true
-    }
-
-    exec { 'phpci-install':
-        command => "console phpci:install --url='${url}' --db-host='${db_host}' --db-name='${db_name}' --db-user='${db_user}' --db-pass='${db_password}' --admin-name='${admin_name}' --admin-mail='${admin_email}' --admin-pass='${admin_password}'",
-        path    => "/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin:${install_path}",
-        cwd => $install_path,
-        user => 'vagrant',
-        subscribe   => Exec['phpci-composer'],
-        refreshonly => true,
-    }
 
     file { "/etc/nginx/sites-enabled/${domain}.conf":
         content => template('phpci/vhost.erb'),
-        ensure => present,
+        ensure => $ensure,
     }
 
     mysql::db { $db_name:
-        ensure   => present,
+        ensure   => $ensure,
         user     => $db_user,
         password => $db_password,
         host     => $db_host,
     }
-
-    Mysql::Db[$db_name] -> Exec['phpci-create']
-
 }
